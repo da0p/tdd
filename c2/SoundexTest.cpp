2b8b66b851283952ddd0ec468010a270b54af223
Phuong Dao <phuong018@gmail.com>
2023-08-22 17:50:37 +0200

    [TDD-02] Simple test

--- c2/SoundexTest.cpp
+++ c2/SoundexTest.cpp
@@ -1,1 +1,19 @@
-#include <gtest/gtest.h>
+#include <string>
+
+class Soundex
+{
+public:
+  std::string encode(std::string const& word) const { return word; }
+};
+
+#include "gmock/gmock.h"
+using ::testing::Eq;
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+{
+  Soundex soundex;
+
+  auto encoded = soundex.encode("A");
+
+  ASSERT_THAT(encoded, Eq("A"));
+}
