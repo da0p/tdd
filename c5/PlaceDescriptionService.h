fc1bf2ecd33bb1fb87b1aae0d309e9e0f5770876
Phuong Dao <phuong018@gmail.com>
2023-09-02 17:39:18 +0200

    [TDD-05] Create AddressExtractor class

--- c5/PlaceDescriptionService.h
+++ c5/PlaceDescriptionService.h
@@ -2,6 +2,16 @@
 #define PLACE_DESCRIPTION_SERVICE_H_
 
 #include "Http.h"
+#include <rapidjson/document.h>
+#include <stdexcept>
+
+class AddressExtractor
+{
+public:
+  AddressExtractor() = default;
+
+  std::string addressFrom(std::string const& jsonResponse) { return ""; }
+};
 
 class PlaceDescriptionService
 {
@@ -16,7 +26,8 @@ public:
     std::string const& longitude
   ) const
   {
-    return "";
+    auto getRequestUrl = "lat=" + lattitude + "&lon=" + longitude;
+    auto jsonResponse = mHttp->get(getRequestUrl);
   }
 
 private:
--- c5/PlaceDescriptionServiceTest.cpp
+++ c5/PlaceDescriptionServiceTest.cpp
@@ -1,9 +1,31 @@
 #include "PlaceDescriptionService.h"
 #include "Http.h"
+#include "gmock/gmock.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+using ::testing::EndsWith;
 using ::testing::Eq;
+
+class AnAddressExtractor : public ::testing::Test
+{
+public:
+  AddressExtractor mAddressExtractor;
+};
+
+TEST_F(AnAddressExtractor, ReturnEmptyStringIfEmptyInput)
+{
+  auto address = mAddressExtractor.addressFrom("");
+
+  ASSERT_THAT(address, Eq(""));
+}
+
+class APlaceDescriptionService : public ::testing::Test
+{
+public:
+  static auto constexpr ValidLattitude{"11.22"};
+  static auto constexpr ValidLongitude{"22.33"};
+};
 
 class HttpStub : public Http
 {
@@ -11,16 +33,19 @@ public:
   void initialize() override {}
   std::string get(std::string const& url) const override
   {
+    verify(url);
     return R"({"address": {"road": "Drury Ln", "city": "Fountain", "state": "CO", "country": "US"}
   }})";
   }
-};
 
-class APlaceDescriptionService : public ::testing::Test
-{
-public:
-  static auto constexpr ValidLattitude{"11.22"};
-  static auto constexpr ValidLongitude{"22.33"};
+  void verify(std::string const& url) const
+  {
+    auto expectedArgs{
+      "lat=" + std::string(APlaceDescriptionService::ValidLattitude) + "&" +
+      "lon=" + std::string(APlaceDescriptionService::ValidLongitude)};
+
+    ASSERT_THAT(url, EndsWith(expectedArgs));
+  }
 };
 
 TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation)
