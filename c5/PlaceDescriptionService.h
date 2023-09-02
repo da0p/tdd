073bc4189f8c0a11982ac750b3a23b4e6e49e7db
Phuong Dao <phuong018@gmail.com>
2023-09-02 16:30:12 +0200

    [TDD-05] Create the PlaceDescriptionService class

--- c5/PlaceDescriptionService.h
+++ c5/PlaceDescriptionService.h
@@ -1,7 +1,18 @@
 #ifndef PLACE_DESCRIPTION_SERVICE_H_
 #define PLACE_DESCRIPTION_SERVICE_H_
 
+#include "Http.h"
+
 class PlaceDescriptionService
-{};
+{
+public:
+  PlaceDescriptionService(Http* http)
+    : mHttp(http)
+  {
+  }
+
+private:
+  Http* mHttp;
+};
 
 #endif
