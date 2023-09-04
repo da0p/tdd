69e2c8af3f0b8af81455127eeaaf040cc871e169
Phuong Dao <phuong018@gmail.com>
2023-09-04 09:35:36 +0200

    [TDD-07] Complete implementation for Portfolio Manager

--- c6/Portfolio.cpp
+++ c6/Portfolio.cpp
@@ -1,26 +1,59 @@
 #include "Portfolio.h"
+#include <stdexcept>
+
+class InvalidPurchaseException : public std::exception
+{
+public:
+  char const* what() const noexcept override
+  {
+    return "Can't purchase 0 share.";
+  }
+};
+
+class InvalidSellException : public std::exception
+{
+public:
+  char const* what() const noexcept override
+  {
+    return "Can't sell more shares than remaining shares.";
+  }
+};
 
 Portfolio::Portfolio()
   : mIsEmpty{true}
-  , mShareCount(0u)
 {
 }
 
 bool
 Portfolio::isEmpty() const
 {
-  return mIsEmpty;
+  return 0 == mHoldings.size();
 }
 
 void
-Portfolio::purchase(std::string const& symbol, unsigned int shareCount)
+Portfolio::purchase(std::string const& symbol, unsigned int shares)
 {
-  mIsEmpty = false;
-  mShareCount = shareCount;
+  if(0 == shares) {
+    throw InvalidPurchaseException();
+  }
+  mHoldings[symbol] = shares + shareCount(symbol);
+}
+
+void
+Portfolio::sell(std::string const& symbol, unsigned int shares)
+{
+  if(shares > shareCount(symbol)) {
+    throw InvalidSellException();
+  }
+  mHoldings[symbol] = shareCount(symbol) - shares;
 }
 
 unsigned int
 Portfolio::shareCount(std::string const& symbol) const
 {
-  return mShareCount;
+  auto it = mHoldings.find(symbol);
+  if(it == mHoldings.end()) {
+    return 0;
+  }
+  return it->second;
 }
--- c6/Portfolio.h
+++ c6/Portfolio.h
@@ -2,18 +2,20 @@
 #define PORTFOLIO_H
 
 #include <string>
+#include <unordered_map>
 
 class Portfolio
 {
 public:
   Portfolio();
   bool isEmpty() const;
-  void purchase(std::string const& symbol, unsigned int shareCount);
+  void purchase(std::string const& symbol, unsigned int shares);
   unsigned int shareCount(std::string const& symbol) const;
+  void sell(std::string const& symbol, unsigned int shares);
 
 private:
   bool mIsEmpty;
-  unsigned int mShareCount;
+  std::unordered_map<std::string, unsigned int> mHoldings;
 };
 
 #endif
