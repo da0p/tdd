7a9c0157521dbfda5f850cd752feb3701871be62
Phuong Dao <phuong018@gmail.com>
2023-09-04 09:17:27 +0200

    [TDD-07] Refactor unit tests

--- c6/PortfolioTest.cpp
+++ c6/PortfolioTest.cpp
@@ -6,8 +6,11 @@ using namespace ::testing;
 class APortfolio : public Test
 {
 public:
+  static const std::string IBM;
   Portfolio mPortfolio;
 };
+
+const std::string APortfolio::IBM{"IBM"};
 
 TEST_F(APortfolio, IsEmptyWhenCreated)
 {
@@ -16,7 +19,7 @@ TEST_F(APortfolio, IsEmptyWhenCreated)
 
 TEST_F(APortfolio, IsNotEmptyAfterPurchase)
 {
-  mPortfolio.purchase("IBM", 1u);
+  mPortfolio.purchase(IBM, 1u);
   ASSERT_FALSE(mPortfolio.isEmpty());
 }
 
@@ -27,6 +30,6 @@ TEST_F(APortfolio, AnswersZeroForShareCountOfUnpurchasedSymbol)
 
 TEST_F(APortfolio, AnswersShareCountForPurchasedSymbol)
 {
-  mPortfolio.purchase("IBM", 2u);
-  ASSERT_THAT(mPortfolio.shareCount("IBM"), Eq(2u));
+  mPortfolio.purchase(IBM, 2u);
+  ASSERT_THAT(mPortfolio.shareCount(IBM), Eq(2u));
 }
